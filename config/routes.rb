35d6666a5ef88c7714f946dd4ff3e3824c00e054
tyosu131 <taisei514@icloud.com>
2022-03-21 20:40:21 +0900

    created user_authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   root 'pages#home'
   get '/about', to: 'pages#about'
 
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
