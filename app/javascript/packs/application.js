db6b97ec757b90d8b3631fab9c5bff475d1a3ca3
Taisei <95160728+tyosu131@users.noreply.github.com>
2022-03-29 00:18:36 +0900

    created Property update (#4)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,5 @@
 import "bootstrap"
+import "@fortawesome/fontawesome-free/css/all"
 
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
